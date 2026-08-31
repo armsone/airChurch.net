d1e245d1df710f51b9c70baaef179c44ec12e793
armsone <armsone@gmail.com>
2026-09-01 02:36:08 +0900

    Use explicit homepage cache policy

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 import Home from "./home-client";
 import type { Metadata } from "next";
 
-export const dynamic="force-static";
-export const revalidate=3600;
 export const metadata:Metadata={alternates:{canonical:"/"}};
 
 export default function Page() {
