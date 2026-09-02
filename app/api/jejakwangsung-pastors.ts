665f031e2bc70c93f906e151e41eabb741e62ff6
armsone <armsone@gmail.com>
2026-09-02 11:55:22 +0900

    Import official Jeja Kwangsung pastors

---
+++ app/api/jejakwangsung-pastors.ts
@@ -0,0 +1,157 @@
+export const jejakwangsungOfficialPastors = [
+  {
+    "name": "박한수",
+    "roleTitle": "담임목사",
+    "roleCategory": "current_primary",
+    "photoUrl": "https://data.dimode.co.kr/UserData/jejaks/files/830/62400/XzAwMTBf67CV7ZWc7IiYLeychOyehOuqqeyCrC5wbmdkaW1vZGVfNzk2NzEyMl9lbmMc.png",
+    "sourceUrl": "https://www.jejaks.org/Page/Index/830"
+  },
+  {
+    "name": "이진원",
+    "roleTitle": "목사",
+    "roleCategory": "associate",
+    "photoUrl": "https://data.dimode.co.kr/UserData/jejaks/files/830/62459/7J207KeE7JuQLeuqqeyCrC5qcGdkaW1vZGVfMzc2MjczMV9lbmMc.jpg",
+    "sourceUrl": "https://www.jejaks.org/Page/Index/830"
+  },
+  {
+    "name": "이경종",
+    "roleTitle": "목사",
+    "roleCategory": "associate",
+    "photoUrl": "https://data.dimode.co.kr/UserData/jejaks/files/830/71477/resized_S2FrYW9UYWxrXzIwMjUxMjI2XzExMTAzMjcwNy5qcGdkaW1vZGVfNzM5MzE2MV9lbmMc.jpg",
+    "sourceUrl": "https://www.jejaks.org/Page/Index/830"
+  },
+  {
+    "name": "서반석",
+    "roleTitle": "목사",
+    "roleCategory": "associate",
+    "photoUrl": "https://data.dimode.co.kr/UserData/jejaks/files/830/65516/resized_7ISc67CY7ISdIOuqqeyCrC5qcGdkaW1vZGVfNTYxNzAyNF9lbmMc.jpg",
+    "sourceUrl": "https://www.jejaks.org/Page/Index/830"
+  },
+  {
+    "name": "선정호",
+    "roleTitle": "목사",
+    "roleCategory": "associate",
+    "photoUrl": "https://data.dimode.co.kr/UserData/jejaks/files/830/72101/7ISg7KCV7Zi4IOuqqeyCrC5wbmdkaW1vZGVfMTk5Nzk3OV9lbmMc.png",
+    "sourceUrl": "https://www.jejaks.org/Page/Index/830"
+  },
+  {
+    "name": "유성경",
+    "roleTitle": "목사",
+    "roleCategory": "associate",
+    "photoUrl": "https://data.dimode.co.kr/UserData/jejaks/files/830/62459/7Jyg7ISx6rK9LeuqqeyCrC5qcGdkaW1vZGVfMzI5MTY4Ml9lbmMc.jpg",
+    "sourceUrl": "https://www.jejaks.org/Page/Index/830"
+  },
+  {
+    "name": "김찬영",
+    "roleTitle": "목사",
+    "roleCategory": "associate",
+    "photoUrl": "https://data.dimode.co.kr/UserData/jejaks/files/830/65549/resized_6rmA7LCs7JiBIOuqqeyCrC5qcGdkaW1vZGVfNjM4NTY4M19lbmMc.jpg",
+    "sourceUrl": "https://www.jejaks.org/Page/Index/830"
+  },
+  {
+    "name": "김지훈",
+    "roleTitle": "목사",
+    "roleCategory": "associate",
+    "photoUrl": "https://data.dimode.co.kr/UserData/jejaks/files/830/62400/OVnquYDsp4Dtm4gt66qp7IKsLnBuZ2RpbW9kZV8yMzg2NjYwX2VuYwcc.png",
+    "sourceUrl": "https://www.jejaks.org/Page/Index/830"
+  },
+  {
+    "name": "이태우",
+    "roleTitle": "목사",
+    "roleCategory": "associate",
+    "photoUrl": "https://data.dimode.co.kr/UserData/jejaks/files/830/65135/7J207YOc7JqwLmpwZ2RpbW9kZV82OTgxNjI3X2VuYwcc.jpg",
+    "sourceUrl": "https://www.jejaks.org/Page/Index/830"
+  },
+  {
+    "name": "이창헌",
+    "roleTitle": "목사",
+    "roleCategory": "associate",
+    "photoUrl": "https://data.dimode.co.kr/UserData/jejaks/files/830/65135/7J207LC97ZeMLmpwZ2RpbW9kZV82NTM3ODQ4X2VuYwcc.jpg",
+    "sourceUrl": "https://www.jejaks.org/Page/Index/830"
+  },
+  {
+    "name": "강요한",
+    "roleTitle": "목사",
+    "roleCategory": "associate",
+    "photoUrl": "https://data.dimode.co.kr/UserData/jejaks/files/830/62400/MTFf6rCV7JqU7ZWcLeyghOuPhOyCrC5wbmdkaW1vZGVfMzIwMTIyNV9lbmMc.png",
+    "sourceUrl": "https://www.jejaks.org/Page/Index/830"
+  },
+  {
+    "name": "이방실",
+    "roleTitle": "목사",
+    "roleCategory": "associate",
+    "photoUrl": "https://data.dimode.co.kr/UserData/jejaks/files/830/62400/7J2067Cp7IukLeyghOuPhOyCrC5wbmdkaW1vZGVfNzcyMzc2Ml9lbmMc.png",
+    "sourceUrl": "https://www.jejaks.org/Page/Index/830"
+  },
+  {
+    "name": "김철우",
+    "roleTitle": "목사",
+    "roleCategory": "associate",
+    "photoUrl": "https://data.dimode.co.kr/UserData/jejaks/files/830/72097/resized_6rmA7LKg7JqwIOuqqeyCrC5qcGdkaW1vZGVfNzY4NTYxNF9lbmMc.jpg",
+    "sourceUrl": "https://www.jejaks.org/Page/Index/830"
+  },
+  {
+    "name": "강성욱",
+    "roleTitle": "목사",
+    "roleCategory": "associate",
+    "photoUrl": "https://data.dimode.co.kr/UserData/jejaks/files/830/71477/resized_S2FrYW9UYWxrXzIwMjUxMjI2XzExMTAzMjcwN18wNC5qcGdkaW1vZGVfOTkzNTIxMl9lbmMc.jpg",
+    "sourceUrl": "https://www.jejaks.org/Page/Index/830"
+  },
+  {
+    "name": "신상운",
+    "roleTitle": "목사",
+    "roleCategory": "associate",
+    "photoUrl": "https://data.dimode.co.kr/UserData/jejaks/files/830/72097/resized_7Iug7IOB7Jq0IOuqqeyCrC5qcGdkaW1vZGVfNTMwMDY1MV9lbmMc.jpg",
+    "sourceUrl": "https://www.jejaks.org/Page/Index/830"
+  },
+  {
+    "name": "손주헌",
+    "roleTitle": "목사",
+    "roleCategory": "associate",
+    "photoUrl": "https://data.dimode.co.kr/UserData/jejaks/files/830/72105/resized_7IaQ7KO87ZeMIOuqqeyCrC5qcGdkaW1vZGVfMTM2NjM3OF9lbmMc.jpg",
+    "sourceUrl": "https://www.jejaks.org/Page/Index/830"
+  },
+  {
+    "name": "임지은",
+    "roleTitle": "전임전도사",
+    "roleCategory": "education",
+    "photoUrl": "https://data.dimode.co.kr/UserData/jejaks/files/830/71477/resized_S2FrYW9UYWxrXzIwMjUxMjI2XzExMTAzMjcwN18wMS5qcGdkaW1vZGVfNDY1MDYzN19lbmMc.jpg",
+    "sourceUrl": "https://www.jejaks.org/Page/Index/830"
+  },
+  {
+    "name": "원예은",
+    "roleTitle": "교육전도사",
+    "roleCategory": "education",
+    "photoUrl": "https://data.dimode.co.kr/UserData/jejaks/files/830/62400/MTZf7JuQ7JiI7J2ALeyghOuPhOyCrC5wbmdkaW1vZGVfODQ0NjM5MV9lbmMc.png",
+    "sourceUrl": "https://www.jejaks.org/Page/Index/830"
+  },
+  {
+    "name": "정대한",
+    "roleTitle": "교육전도사",
+    "roleCategory": "education",
+    "photoUrl": "https://data.dimode.co.kr/UserData/jejaks/files/830/65135/7KCV64yA7ZWcLmpwZ2RpbW9kZV82ODMzNzMzX2VuYwcc.jpg",
+    "sourceUrl": "https://www.jejaks.org/Page/Index/830"
+  },
+  {
+    "name": "이진숙",
+    "roleTitle": "전도사",
+    "roleCategory": "education",
+    "photoUrl": "https://data.dimode.co.kr/UserData/jejaks/files/830/71477/resized_7J207KeE7IiZIOyImOyglS5qcGdkaW1vZGVfODQ2MTc1NF9lbmMc.jpg",
+    "sourceUrl": "https://www.jejaks.org/Page/Index/830"
+  },
+  {
+    "name": "여호수아",
+    "roleTitle": "전도사",
+    "roleCategory": "education",
+    "photoUrl": "https://data.dimode.co.kr/UserData/jejaks/files/830/71477/resized_7Jes7Zi47IiY7JWE7IiY7KCVLmpwZ2RpbW9kZV84MDYxNDEzX2VuYwcc.jpg",
+    "sourceUrl": "https://www.jejaks.org/Page/Index/830"
+  },
+  {
+    "name": "김병국",
+    "roleTitle": "찬양전도사",
+    "roleCategory": "education",
+    "photoUrl": "https://data.dimode.co.kr/UserData/jejaks/files/830/62400/6rmA67OR6rWtLeyghOuPhOyCrC5wbmdkaW1vZGVfNjE4NjczMV9lbmMc.png",
+    "sourceUrl": "https://www.jejaks.org/Page/Index/830"
+  }
+] as const;
+
